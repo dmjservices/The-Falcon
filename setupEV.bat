6cca90da9152c28a31f22402c61426e0cc72be43
dmjservices <mrdmjservices@gmail>
2021-12-28 12:57:05 -0800

    Update Setup Bat file

--- setupEV.bat
+++ setupEV.bat
@@ -8,20 +8,20 @@
 setx PATH_DSS_Repo "%USERPROFILE%\Documents\Trading_Repository"
 
 ::# Customize path to the user folders with Trading Platforms
-setx PATH_T1 "C:\OANDA - MetaTrader Portable - (1)\MQL4\Files"
-setx PATH_T1_E "C:\OANDA - MetaTrader Portable - (1)\MQL4\Experts"
-setx PATH_T1_I "C:\OANDA - MetaTrader Portable - (1)\MQL4\Include"
+setx PATH_T1 "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (1)\MQL4\Files"
+setx PATH_T1_E "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (1)\MQL4\Experts"
+setx PATH_T1_I "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (1)\MQL4\Include"
 
-setx PATH_T2 "C:\OANDA - MetaTrader Portable - (2)\MQL4\Files"
-setx PATH_T2_E "C:\OANDA - MetaTrader Portable - (2)\MQL4\Experts"
-setx PATH_T2_I "C:\OANDA - MetaTrader Portable - (2)\MQL4\Include"
+setx PATH_T2 "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (2)\MQL4\Files"
+setx PATH_T2_E "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (2)\MQL4\Experts"
+setx PATH_T2_I "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (2)\MQL4\Include"
 
-setx PATH_T3 "C:\OANDA - MetaTrader Portable - (3)\MQL4\Files"
-setx PATH_T3_E "C:\OANDA - MetaTrader Portable - (3)\MQL4\Experts"
-setx PATH_T3_I "C:\OANDA - MetaTrader Portable - (3)\MQL4\Include"
+setx PATH_T3 "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (3)\MQL4\Files"
+setx PATH_T3_E "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (3)\MQL4\Experts"
+setx PATH_T3_I "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (3)\MQL4\Include"
 
-setx PATH_T4 "C:\OANDA - MetaTrader Portable - (4)\MQL4\Files"
-setx PATH_T4_E "C:\OANDA - MetaTrader Portable - (4)\MQL4\Experts"
-setx PATH_T4_I "C:\OANDA - MetaTrader Portable - (4)\MQL4\Include"
+setx PATH_T4 "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\MQL4\Files"
+setx PATH_T4_E "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\MQL4\Experts"
+setx PATH_T4_I "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\MQL4\Include"
 
 pause
